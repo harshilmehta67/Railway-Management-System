8060dc04dac16e1f3b48af54566d81d04b08b541
YashilDepani <51193749+YashilDepani@users.noreply.github.com>
2020-04-11 22:14:22 +0530

    cancel ticket

--- database1.py
+++ database1.py
@@ -1,80 +1,71 @@
 import psycopg2
 
-# try:
-#     connection = psycopg2.connect(user = "postgres",
-#                                   password = "yashilpostgresql",
-#                                   host='localhost',
-#                                   port = "5432",
-#                                   database = "postgres")
-#     cursor = connection.cursor()
-#     user_id=1103
-#     user_name='p'
-#     emailid = 'e'
-#     gender = 'F'
-#     dob = '12-12-2009'
-#     mobile_no = 108349
-#     cursor.execute("insert into user1 values(%s,%s,%s,%s,%s,%s)", (user_id, emailid,user_name,gender,dob,mobile_no))
-#     #connection.commit()
-#     print("success  Fecthing Values")
+try:
+    connection = psycopg2.connect(user = "postgres",
+                                  password = "yashilpostgresql",
+                                  host='localhost',
+                                  port = "5432",
+                                  database = "postgres")
+    cursor = connection.cursor()
+    user_id=1103
+    user_name='p'
+    emailid = 'e'
+    gender = 'F'
+    dob = '12-12-2009'
+    mobile_no = 108349
+    #cursor.execute("insert into user1 values(%s,%s,%s,%s,%s,%s)", (user_id, emailid,user_name,gender,dob,mobile_no))
+    #connection.commit()
+    #this function inserts values into table user1
+    print("success  Fecthing Values")
 
-# except (Exception, psycopg2.Error) as error :
-#     print ("Error while connecting to PostgreSQL", error)
+    query = "select * from train_btwn_date(%s,%s)"
+    #this is my inbuit fuction which takes two input date as paramter and gives output as a tables  
+    #the function given below
+    f_date = '11-04-2020'
+    t_date = '13-04-2020'
+    cursor.execute(query,(f_date,t_date))
+    train_name = cursor.fetchall()
+    for i in train_name:
+       print(train_name)
 
-# finally:
-#     if(connection):
-#         cursor.close()
-#         connection.close()
-#         print("PostgreSQL connection is closed")
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+
+finally:
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
 #link where i got this from :https://pynative.com/python-postgresql-tutorial/
-#query for day from date select extract(dow from date '08-12-2019');
-connection = psycopg2.connect(user = "postgres",
-                              password = "yashilpostgresql",
-                              host='localhost',
-                              port = "5432",
-                              database = "postgres")
-cursor = connection.cursor()
-user_id=1103
-user_name='p'
-emailid = 'e'
-gender = 'F'
-dob = '12-12-2009'
-mobile_no = 108349
-trian_id = "10010"
-station_id = 111
-#cursor.execute("DELETE FROM station1 WHERE station1.station_id = %s",(station_id,))
-#cursor.execute("DELETE FROM train1 WHERE train1.train_id = 10010")
-#connection.commit()
-sp    = 100
-ep    = 101
 
+create or replace function train_btwn_date(f_date date, t_date date)
+returns table(
+t_id int,
+t_name varchar(20),
+s_station int,
+e_station int,
+day_int int
+)
+as $train_btwn_date$
+declare
+day1 int;
+day2 int;
+begin
+select extract(dow from f_date) into day1;
+select extract(dow from t_date) into day2;
+if(day1<day2) then
+return query select distinct train1.train_id,train1.train_name,train1.starting_station_id,
+train1.ending_station_id,seat_class2.working_day from train1,seat_class2
+where seat_class2.working_day >= day1 and seat_class2.working_day <= day2;
+else
+return query select distinct train1.train_id,train1.train_name,train1.starting_station_id,
+train1.ending_station_id,seat_class2.working_day from train1,seat_class2
+where seat_class2.working_day >= day2 and seat_class2.working_day <= 7 and
+seat_class2.working_day >= 0 and seat_class2.working_day <= day1;
+end if;
+end;
+$train_btwn_date$
+language plpgsql
 
-#connection.commit()
-query = "select train_btwn_date(%s,%s)"
-f_date = '11-04-2020'
-t_date = '13-04-2020'
-cursor.execute(query,(f_date,t_date))
-train_name = cursor.fetchall()
-
-
-user_id = 1100
-query = "select name from user1 where user_id=%s"
-cursor.execute(query,(user_id,))
-user_name = cursor.fetchone()
-print(user_name)
-user_name = user_name[0]
-
-train_id = 10000
-divas = 1
-seat_category = 'AC1'
-
-query = "select book_seat(%s,%s,%s)"
-cursor.execute(query,(train_id,divas,seat_category))
-ticketFair = cursor.fetchone()
-if int(ticketFair[0]) > 100000 :
-   query = "insert into passenger1 values(%s,nextval('pnr_seq'),%s,%s,%s,%s,'confirm')"
-   cursor.execute(query,(user_id,user_name,train_id,divas,seat_category,))
-   connection.commit()
-else:
-   query = "insert into passenger1 values(%s,nextval('pnr_seq'),%s,%s,%s,%s,'reserved')"
-   cursor.execute(query,(user_id,user_name,train_id,divas,seat_category,))
-   connection.commit()
+select train_btwn_date('11-04-2020','15-04-2020')
